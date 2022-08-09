22ec7ffa365374e0029f23f7029fe8f0ba90bd0f
Jacques Lombaard <lombaardj@gmail.com>
2022-08-09 15:52:57 +0200

    chore: commit all

--- WeatherFunction/WeatherFunction/Weather.cs
+++ WeatherFunction/WeatherFunction/Weather.cs
@@ -7,7 +7,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-
 namespace WeatherFunction
 {
     public static class Weather
